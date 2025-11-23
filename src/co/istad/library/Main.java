0524adab6dcb07fb9e905419c4fc39c986aaff64
PisethMao <pisethmao2002@gmail.com>
2025-11-23 23:59:56 +0700

    setup project with package and banner already

---
+++ src/co/istad/library/Main.java
@@ -0,0 +1,9 @@
+package co.istad.library;
+
+import co.istad.library.util.ViewUtil;
+
+public class Main {
+    public static void main(String[] args) {
+        ViewUtil.printBanner();
+    }
+}
---
+++ src/co/istad/library/util/Color.java
@@ -0,0 +1,18 @@
+package co.istad.library.util;
+
+public class Color {
+    public static final String RESET = "\033[0m";
+    public static final String RED = "\033[0;31m";
+    public static final String GREEN = "\033[0;32m";
+    public static final String BLUE = "\033[0;34m";
+    public static final String YELLOW = "\033[0;33m";
+    public static final String CYAN = "\033[0;36m";
+    public static final String MAGENTA = "\033[0;35m";
+    public static final String WHITE = "\033[0;37m";
+    public static final String PURPLE = "\033[0;35m";
+    public static final String BLACK = "\033[0;30m";
+    public static final String GRAY = "\033[0;90m";
+    public static final String BOLD = "\033[1m";
+    public static final String UNDERLINE = "\033[4m";
+    public static final String BOLD_CYAN = BOLD + CYAN;
+}
